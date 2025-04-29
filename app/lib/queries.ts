8ade4f13a2fb272e780fab47adce70b33ef66b39
evefonwu <evefonwu@gmail.com>
2025-04-28 23:26:40 -0400

    fix type definition

--- app/lib/queries.ts
+++ app/lib/queries.ts
@@ -1,13 +1,13 @@
 import postgres from "postgres";
 
-import { Location } from "./definitions";
+import { UserSavedLocation } from "./definitions";
 
 const sql = postgres(process.env.DATABASE_URL!, { ssl: "require" });
 
 export async function fetchLocations() {
   console.log("Getting user locations data from database...");
   try {
-    const data = await sql<Location[]>`SELECT * from userLocations`;
+    const data = await sql<UserSavedLocation[]>`SELECT * from userLocations`;
     return data;
   } catch (error) {
     console.error("database error:", error);
