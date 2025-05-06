440b8cfa9861c703f53f7d9290e31fa568811497
evefonwu <evefonwu@gmail.com>
2025-05-06 15:47:38 -0400

    catch schema parse error

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -18,15 +18,15 @@ const CreateLocation = FormSchema.omit({ id: true, date: true });
 export async function createLocation(formData: FormData) {
   console.log(formData);
 
-  const { nickname, location, zip } = CreateLocation.parse({
-    nickname: formData.get("nickname"),
-    location: formData.get("location"),
-    zip: formData.get("zip"),
-  });
-
-  const date = new Date().toISOString().split("T")[0];
-
   try {
+    const { nickname, location, zip } = CreateLocation.parse({
+      nickname: formData.get("nickname"),
+      location: formData.get("location"),
+      zip: formData.get("zip"),
+    });
+
+    const date = new Date().toISOString().split("T")[0];
+
     await sql`
         INSERT INTO userLocations (nickname, location, zipcode, date)
         VALUES (${nickname}, ${location}, ${zip}, ${date})
